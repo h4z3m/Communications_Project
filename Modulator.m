78d4954263b9c6343851c970d4285ce6295ffee6
h4z3m <h4z3m.private@gmail.com>
2023-12-02 17:15:22 +0200

    Modulators

--- Modulator.m
+++ Modulator.m
@@ -10,6 +10,61 @@ classdef Modulator
 
     methods
 
+        function decoded_value = decode(obj, T, f_c, E, symbol, t, x)
+
+            if obj.type == ModulationTypes.QAM16
+                % Set the input values ai and bi based on the current bit
+                if symbol(1) == 0 && symbol(2) == 0
+                    ai = -3;
+                elseif symbol(1) == 0 && symbol(2) == 1
+                    ai = -1;
+                elseif symbol(1) == 1 && symbol(2) == 1
+                    ai = 1;
+                else
+                    ai = 3;
+                end
+
+                if symbol(3) == 0 && symbol(4) == 0
+                    bi = 3;
+                elseif symbol(3) == 0 && symbol(4) == 1
+                    bi = 1;
+                elseif symbol(3) == 1 && symbol(4) == 1
+                    bi = -1;
+                else
+                    bi = -3;
+                end
+
+                decoded_value = obj.signal_fn(ai, bi, t + x * T, T, f_c, E);
+            else
+
+                bitsString = char(symbol + '0');
+                integer = bin2dec(bitsString);
+                decoded_value = obj.signal_fn(integer, t, T, f_c, E, obj.M);
+            end
+
+        end
+
+        function modulated_bits = modulate(obj, T, f_c, E, bits)
+
+            k = log2(obj.M); % Number of bits per symbol
+
+            modulated_bits = zeros(floor(length(bits) / k), 1);
+            x = 1;
+            y = 1;
+
+            for i = 1:k:(length(bits) - mod(length(bits), k))
+                %% Decode the bits
+                for t = 0:0.05:T
+                    % Calculate the signal value for the current bit
+                    modulated_bits(y) = obj.decode(T, f_c, E, bits(i:i + k - 1), t, x);
+                    y = y + 1;
+                end
+
+                x = x + 1;
+            end
+
+        end
+
         function obj = Modulator(type)
             obj.type = type;
 
--- test.m
+++ test.m
@@ -1,50 +1,68 @@
-import Modulator.*
-% Define the time period
-start_time = 0;
-end_time = 1; % Modify this according to your desired time period
-time_step = 0.01; % Adjust the time step as needed
+clc
+close all
+clear
 
-% Create an array of time points
-t = start_time:time_step:end_time;
-
-% Create an array to store the signal values
-signal_values = zeros(size(t));
-
-% Set the input values for the signal function
-ai = 0.5; % Modify as needed
-bi = 0.3; % Modify as needed
+% Define random sequence of bits for testing of size 48k
+bits = [1 0 1 0 0 1 0 0 1 1 1 1 0 0 0 1 1 0 0 0];
+% bits = randi([0 1], 1, 48 * 1000);
+% % Set the input values for the signal function
 T = 1; % Modify as needed
-f_c = 10; % Modify as needed
-E = 2; % Modify as needed
+f_c = 3; % Modify as needed
+E = 5; % Modify as needed
+M = 16;
 
-% Evaluate the signal function for each time point
-for i = 1:numel(t)
-    signal_values(i) = modulator.signal_fn(ai, bi, t(i), T, f_c, E);
-end
+% % Create an array to store the signal values for each bit
+% signal_values = zeros(length(bits) / 4, 1);
+% a = zeros(length(bits) / 4, 1);
+% b = zeros(length(bits) / 4, 1);
 
-% Plot the signal over the time period
-plot(t, signal_values);
-xlabel('Time');
-ylabel('Signal');
-title('Signal over Time');
-testModulatorSignalFn();
+% modulator = Modulator(ModulationTypes.QAM16);
+% x = 1;
+% y = 1;
+% % Evaluate the signal function for each bit
+% for i = 1:4:numel(bits)
+%     % Set the input values ai and bi based on the current bit
+%     if bits(i) == 0 && bits(i + 1) == 0
+%         ai = -3;
+%     elseif bits(i) == 0 && bits(i + 1) == 1
+%         ai = -1;
+%     elseif bits(i) == 1 && bits(i + 1) == 1
+%         ai = 1;
+%     else
+%         ai = 3;
+%     end
 
-function testModulatorSignalFn()
-    % Create an instance of the Modulator class
-    modulator = Modulator(ModulationTypes.QAM16);
+%     if bits(i + 2) == 0 && bits(i + 3) == 0
+%         bi = 3;
+%     elseif bits(i + 2) == 0 && bits(i + 3) == 1
+%         bi = 1;
+%     elseif bits(i + 2) == 1 && bits(i + 3) == 1
+%         bi = -1;
+%     else
+%         bi = -3;
+%     end
 
-    % Set input values for the signal function
-    ai = 0.5;
-    bi = 0.3;
-    t = 0.1;
-    T = 1;
-    f_c = 10;
-    E = 2;
+%     for t = 0:0.05:T
+%         % Calculate the signal value for the current bit
+%         signal_values(y) = modulator.signal_fn(ai, bi, t + x * T, T, f_c, E);
+%         y = y + 1;
+%     end
 
-    % Call the signal function
-    result = modulator.signal_fn(ai, bi, t, T, f_c, E);
-    disp(result);
-    % Verify the output
-    expected = sqrt(E) * ai * modulator.phi_1(t, T, f_c) - sqrt(E) * bi * modulator.phi_2(t, T, f_c);
-    assert(isequal(result, expected), 'Signal function output is incorrect');
-end
+%     a(x) = ai;
+%     b(x) = bi;
+%     x = x + 1;
+% end
+
+% % Display the resulting signal values for each bit
+% disp(signal_values);
+% fig = figure();
+
+% stairs(a, 'LineWidth', 2);
+% figure();
+% stairs(b, 'LineWidth', 2);
+% figure();
+
+modulator = Modulator(ModulationTypes.PSK8);
+signal_values = modulator.modulate(T, f_c, E, bits);
+time = linspace(0, T, length(signal_values));
+plot(time, signal_values, 'LineWidth', 2);
